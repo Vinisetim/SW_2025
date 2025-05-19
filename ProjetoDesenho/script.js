8ad5500dd7371bbc40e83241ed16c12342dd7465
Vinisetim <viniciusgueimis@gmail.com>
2025-05-19 10:49:22 -0300

    Add files via upload

---
+++ ProjetoDesenho/script.js
@@ -0,0 +1,14 @@
+function exibirNome(){
+    let nome = document.getElementById('nomeInput').value;
+    if (nome ==="Saber"){
+        document.getElementById('mensagem').innerHTML = "Você acertou meu nome, parabéns!";
+    }else{
+        document.getElementById('mensagem').innerHTML = "Você errou meu nome, tente novamente!";
+    }
+}
+
+function limpar(){
+    document.getElementById('nomeInput').value = "";
+    document.getElementById('mensagem').innerHTML = "";
+    document.getElementById('nomeInput').focus();
+}
