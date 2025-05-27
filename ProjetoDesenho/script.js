85e9b811627eb17283f6cbf0f03aa3d4bb223020
Vinisetim <viniciusgueimis@gmail.com>
2025-05-27 17:46:22 -0300

    Add files via upload

--- ProjetoDesenho/script.js
+++ ProjetoDesenho/script.js
@@ -1,6 +1,6 @@
 function exibirNome(){
     let nome = document.getElementById('nomeInput').value;
-    if (nome ==="Saber"){
+    if (nome ==="cadu maverick"){
         document.getElementById('mensagem').innerHTML = "Você acertou meu nome, parabéns!";
     }else{
         document.getElementById('mensagem').innerHTML = "Você errou meu nome, tente novamente!";
